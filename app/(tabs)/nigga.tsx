488b09d26dec7ed849bf58034a9e4b96eddc487e
musxeto <mustafamalikawan786@gmail.com>
2024-08-07 21:13:22 +0500

    Initials

---
+++ app/(tabs)/nigga.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import React, { Component } from "react";
+
+const Nigga = () => {
+  return (
+    <View>
+      <Text>nigga</Text>
+    </View>
+  );
+};
+
+export default Nigga
---
+++ components/PopolarJobs.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const PopolarJobs = () => {
+  return (
+    <View>
+      <Text>PopolarJobs</Text>
+    </View>
+  )
+}
+
+export default PopolarJobs
